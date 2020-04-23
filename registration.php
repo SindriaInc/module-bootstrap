e27c01cbf86dea48bee64f5719aea8bacb45b614
lucapitzoi <luca.pitzoi@sindria.org>
2020-04-23 16:58:14 +0200

    Edit copyright

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright © Sindria, Inc. All rights reserved.
+ * See LICENSE for license details.
  */
 
 use \Magento\Framework\Component\ComponentRegistrar;
